734cc0f6a548934e9ddda3e7c9d35d84966849f0
luis-pedro-dutra-carrocini <luispedrodutracarrocini58@gmail.com>
2025-10-31 20:07:26 -0300

    feat(login): Adicionar novo Título do Sistema

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -96,7 +96,7 @@ function Login() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Vio
+          Vio Testando um Deploy Ao Vivo e a Cores
         </Typography>
         <Box
           component="form"
